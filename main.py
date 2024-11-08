e6aa06a8a02fe914d8a4d4cdcfa6110761473d89
Laython Childers <laythonchilders5@gmail.com>
2024-11-08 12:49:37 -0500

    Init commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import pygame
+import sys
+from settings import *
+from map import * 
+from player import *
+from raycasting import *
+
+class CatDoom:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(RESOLUTION)
+        self.clock = pygame.time.Clock()
+        self.delta_time = 1
+        self.new_game() 
+
+    def new_game(self):
+        self.map = Map(self)
+        self.player = Player(self)
+        self.raycasting = Raycasting(self)
+
+    def draw_frame(self): #called in loop to update/redraw each frame
+        self.player.update()
+        self.raycasting.update()
+        pygame.display.flip()
+        self.delta_time = self.clock.tick(FPS)
+        pygame.display.set_caption(f' {self.clock.get_fps() : .1f}')
+
+    def draw(self):
+        self.screen.fill('black')
+        #self.map.draw()
+        #self.player.draw()
+
+
+    def check_exit(self): # checks for events/keystrokes that signal user intent to exit
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+
+    def main_loop(self):
+        while True:
+            self.check_exit()
+            self.draw_frame()
+            self.draw()
+
+if __name__ == '__main__':
+    catGame = CatDoom()
+    catGame.main_loop() 
---
+++ player.py
@@ -0,0 +1,68 @@
+from settings import *
+import pygame as pygame
+import math
+
+
+class Player:
+    def __init__ (self, catGame):
+        self.catGame = catGame
+        self.x, self.y = PLAYER_POS
+        self.angle = PLAYER_ANGLE
+
+    def move(self):
+        sin_a = math.sin(self.angle)
+        cos_a = math.cos(self.angle)
+        dx, dy = 0, 0
+        speed = PLAYER_SPEED * self.catGame.delta_time
+        speed_sin = speed * sin_a
+        speed_cos = speed * cos_a
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_w]:
+            dx += speed_cos
+            dy += speed_sin
+        if keys[pygame.K_s]:
+            dx += -speed_cos
+            dy += -speed_sin
+        if keys[pygame.K_a]:
+            dx += speed_sin
+            dy += -speed_cos
+        if keys[pygame.K_d]:
+            dx += -speed_sin
+            dy += speed_cos
+
+        self.check_wall_collison(dx, dy)
+
+        if keys[pygame.K_LEFT]:
+            self.angle -= PLAYER_ROT_SPEED * self.catGame.delta_time
+        if keys[pygame.K_RIGHT]:
+            self.angle += PLAYER_ROT_SPEED * self.catGame.delta_time
+        self.angle %= math.tau
+
+    def check_wall(self, x, y):
+        return (x, y) not in self.catGame.map.world_map
+    
+    def check_wall_collison(self, dx, dy):
+        if self.check_wall(int(self.x + dx), int(self.y)):
+            self.x += dx
+        if self.check_wall(int(self.x), int(self.y + dy)):
+            self.y += dy
+
+
+
+    def draw(self):
+        #pygame.draw.line(self.catGame.screen, 'yellow', (self.x * 100, self.y * 100),
+        #            (self.x * 100 + WIDTH * math.cos(self.angle),
+        #             self.y * 100 + WIDTH * math. sin(self.angle)), 2)
+        pygame.draw.circle(self.catGame.screen, 'green', (self.x * 100, self.y * 100), 15)
+
+    def update(self):
+        self.move()
+
+    @property
+    def pos(self):
+        return self.x, self.y
+    @property
+    def map_pos(self):
+        return int(self.x), int(self.y)
