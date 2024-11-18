047fb4b3ae2eb8ec9af6e55c4ac5ba009e09da75
Laython Childers <laythonchilders5@gmail.com>
2024-11-18 13:40:52 -0500

    Attempt fix mouse not grabbed

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ class CatDoom:
         pygame.init()
         # not working????
         pygame.mouse.set_visible(False)
+        # fixes mouse not captured possibly
+        pygame.event.set_grab(True)
         self.screen = pygame.display.set_mode(RESOLUTION)
         self.clock = pygame.time.Clock()
         self.delta_time = 1
