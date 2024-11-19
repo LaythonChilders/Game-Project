bccc03b10df351057de8feab999548a2843edc63
DaVisser <visserdaniel9@gmail.com>
2024-11-19 13:36:28 -0500

    weapon foxing

--- weapon.py
+++ weapon.py
@@ -1,10 +1,11 @@
 from sprite_object import *
 
-# Inherits from AnimatedSprite class while specificying only the path, scale, and time
-# of the animation
+
 class Weapon(AnimatedSprite):
     def __init__(self, game, path='Resources/Sprites/Weapon/Shotgun/0.png', scale=0.4, animation_time=90):
         super().__init__(game=game, path=path, scale=scale, animation_time=animation_time)
+        # Fix for starting weapon frame
+        self.images.rotate(4)
         self.images = deque(
             [pg.transform.smoothscale(img, (self.image.get_width() * scale, self.image.get_height() * scale))
              for img in self.images])
@@ -12,14 +13,14 @@ class Weapon(AnimatedSprite):
         self.reloading = False
         self.num_images = len(self.images)
         self.frame_counter = 0
-        self.damage = 20
+        self.damage = 50
 
     def animate_shot(self):
         if self.reloading:
             self.game.player.shot = False
             if self.animation_trigger:
                 self.images.rotate(-1)
-                self.images = self.images[0]
+                self.image = self.images[0]
                 self.frame_counter += 1
                 if self.frame_counter == self.num_images:
                     self.reloading = False
